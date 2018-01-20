2556242b0b30e1efafbb3d481dd3ef86abd1d47a
Zushisan <74838@supinfo.com>
2018-01-20 04:38:49 +0000

    testing and backup before other method attempt, some test are failing.

--- stock-market.js
+++ stock-market.js
@@ -1,3 +1,6 @@
+var balancing = 1; // balancing is a global variable that we want to keep track of in each function
+
+
 // FIRST FUNCTION THAT REMOVE ANY MAX VAR ON THE LEFT AND MIN ON THE RIGHT
 // MATHEMATICALY IMPOSSBILE VALUE
 function removeEdges(array){
@@ -64,7 +67,16 @@ function bestProfit(array){
     if(stepMax !== false){
       return (maxSolution(stepMax, step1));
     }
-    step1 = step1.slice(1);
+
+    if(balancing === 1){
+      step1 = step1.slice(1);
+      balancing = 2;
+    }
+    else {
+      step1 = step1.slice(0, -1);
+      balancing = 1;
+    }
+
   }
 }
 
@@ -83,6 +95,16 @@ var testArray2 = [1, 3, 7, 9, 11, 14, 19]; // 18
 var testArray3 = [19, 14, 11, 9, 7, 3, 1]; // no profit
 var testArray4 = [45, 4, 48, 31, 7, 38, 11]; // 44
 var testArray5 = [45, 4, 48, 1, 78, 38, 11]; // 77
+var testArray6 = [45, 1, 48, 4, 78, 38, 11]; // 77
+var testArray7 = [11, 10, 51, 7, 50, 1, 2, 3]; // 43
+
+
+
+var testArray8 = [154, 25, 76, 179, 83, 181, 44, 27, 102, 133, 77, 162, 81, 101, 22, 89, 113, 100, 183, 99]; // 183-22 161
+var testArray9 = [195, 31, 111, 63, 191, 120, 9, 144, 177, 125, 178, 180, 179, 134, 130, 24, 42, 88, 153, 99]; // 180-9 171
+var testArray10 = [50, 6, 125, 133, 192, 32, 123, 44, 159, 93, 166, 193, 176, 31, 182, 49, 97, 55, 42, 114]; // 187
+var testArray11 = [122, 728, 124, 66, 149, 160, 102, 121, 33, 94, 48, 162, 161, 155, 9, 163, 175, 74, 96, 44]; // 728-122 606 <=== FAIL
+var testArray12 = [124, 29, 140, 51, 45, 160, 105, 27, 104, 30, 35, 161, 117, 121, 58, 125, 127, 177, 168, 2]; // 177-27 150
 
 
 message(bestProfit(testArray));
@@ -90,6 +112,12 @@ message(bestProfit(testArray2));
 message(bestProfit(testArray3));
 message(bestProfit(testArray4));
 message(bestProfit(testArray5));
+message(bestProfit(testArray6));
+message(bestProfit(testArray7));
 
-
+message(bestProfit(testArray8));
+message(bestProfit(testArray9));
+message(bestProfit(testArray10));
+message(bestProfit(testArray11));
+message(bestProfit(testArray12));
 // TODO: TEST FUNCTION, REFACTOR THE WAY OF CALLING: message(bestProfit(testArray)); <===
